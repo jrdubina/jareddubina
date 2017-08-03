bb25ca691b4a11d6a7f0e316ac16ff15df22cc23
Jared Dubina <jrdubina@gmail.com>
2017-08-03 13:53:02 -0700

    Working In react

--- reactjs-basics/src/app/index.js
+++ reactjs-basics/src/app/index.js
@@ -1,1 +1,14 @@
-console.log("It Works!");
+import React from "react";
+import {render} from "react-dom";
+
+class FirstApp extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
+}
+
+render(<FirstApp/>, window.document.getElementByID("FirstApp"));
