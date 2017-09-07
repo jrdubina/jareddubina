e6c909a698f627ec37f7871c8080986289f8d6cf
jareddGoWise <30871001+jareddGoWise@users.noreply.github.com>
2017-09-07 11:45:28 -0700

    Updated site

---
+++ agenttool/agenttool/Start.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    	<link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>Agent Authentication</title>
+	</head>
+	<body onload="displaydata();">
+	<center>
+	<img src="http://jareddubina.com/agenttool/Connection.png" />
+		<h3>Please choose to generate a code or validate a code given to you</h3>
+			<form action="http://jareddubina.com/agenttool/Captcha.php" method="get">
+			<input type="image" src="http://jareddubina.com/agenttool/Generate.png" /></form> <br />
+
+			<form action="http://jareddubina.com/agenttool/Submit.php">
+			<input type="image" src="http://jareddubina.com/agenttool/Validate.png" /></form>
+    </center>
+	</body>
+</html>
---
+++ agenttool/agenttool/Submit.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    	<link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>Authentication Request</title>
+	</head>
+	<body><center>
+	<img src="http://jareddubina.com/agenttool/Connection.png" />
+		<h3>Please enter the agents code below</h3>
+		<form action="http://jareddubina.com/agenttool/Search.php" method="get">
+			<h3>Enter Code Here:</h3><input type="text" size="12" maxlength="6" name="code"><br><br>
+		<input type="submit" value="Check Code">
+    </form>
+	</center>
+	</body>
+</html>
